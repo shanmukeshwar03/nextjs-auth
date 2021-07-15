c330dcc44e054d85d55416b0933910d9584dcefc
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-07-15 18:33:30 +0530

    test

--- fetch/auth.js
+++ fetch/auth.js
@@ -1,4 +1,4 @@
-const BASE = 'https://auth.shanmukeshwar.ml/'
+const BASE = 'https://auth.shnm.ml/'
 
 export const login = async (payload) => {
   const response = await fetch(BASE + 'login', {
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,20 @@ import 'styles/globals.css'
 import 'styles/form.css'
 
 const App = ({ Component }) => {
-  return <Component />
+  return (
+    <>
+      <Component />
+      <button
+        onClick={async () => {
+          const response = await fetch('https://test.shnm.ml')
+          const res = await response.json()
+          console.log(res)
+        }}
+      >
+        test
+      </button>
+    </>
+  )
 }
 
 export default App
