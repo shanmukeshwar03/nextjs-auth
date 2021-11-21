0b5081d9366cc281da7e6a0606006330ecbd64a0
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-11-21 21:38:51 +0530

    some improvements

--- fetch/auth.js
+++ fetch/auth.js
@@ -1,57 +1,57 @@
-const BASE = 'https://auth.shanmukeshwar.me/'
+const BASE = "https://auth.shanmukeshwar.me/";
 
 export const login = async (payload) => {
-  const response = await fetch(BASE + 'login', {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'post',
+  const response = await fetch(BASE + "login", {
+    headers: { "Content-Type": "application/json" },
+    method: "post",
     body: JSON.stringify(payload),
-    credentials: 'include'
+    credentials: "include",
   }).then(async (res) => {
-    if (res.ok) return { success: true }
-    else return { failed: await res.text() }
-  })
+    if (res.ok) return { success: true };
+    else return { failed: await res.text() };
+  });
 
-  return response
-}
+  return response;
+};
 
 export const register = async (payload) => {
-  const response = await fetch(BASE + 'register', {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'post',
+  const response = await fetch(BASE + "register", {
+    headers: { "Content-Type": "application/json" },
+    method: "post",
     body: JSON.stringify(payload),
-    credentials: 'include'
+    credentials: "include",
   }).then(async (res) => {
-    if (res.ok) return { success: true }
-    else return { failed: await res.text() }
-  })
+    if (res.ok) return { success: true };
+    else return { failed: await res.text() };
+  });
 
-  return response
-}
+  return response;
+};
 
 export const forgotpassword = async (payload) => {
-  const response = await fetch(BASE + 'forgotpassword', {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'post',
+  const response = await fetch(BASE + "forgotpassword", {
+    headers: { "Content-Type": "application/json" },
+    method: "post",
     body: JSON.stringify(payload),
-    credentials: 'include'
+    credentials: "include",
   }).then(async (res) => {
-    if (res.ok) return { success: true }
-    else return { failed: await res.text() }
-  })
+    if (res.ok) return { success: true };
+    else return { failed: await res.text() };
+  });
 
-  return response
-}
+  return response;
+};
 
 export const resetpassword = async (payload, token) => {
-  const response = await fetch(BASE + 'resetpassword/' + token, {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'PATCH',
+  const response = await fetch(BASE + "resetpassword/" + token, {
+    headers: { "Content-Type": "application/json" },
+    method: "PATCH",
     body: JSON.stringify(payload),
-    credentials: 'include'
+    credentials: "include",
   }).then(async (res) => {
-    if (res.ok) return { success: true }
-    else return { failed: await res.text() }
-  })
+    if (res.ok) return { success: true };
+    else return { failed: await res.text() };
+  });
 
-  return response
-}
+  return response;
+};
